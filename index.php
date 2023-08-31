e154da1329906eb0eba374cc68b2959fd05b3983
Sayed Suliman Sadat <109897633+sulimansadat@users.noreply.github.com>
2023-08-31 14:37:57 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,301 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Health Service | Find Doctors in Afghanistan">
+    <title>Health Service | Home Page By Suleman Sadat</title>
+
+    <link rel="shortcut icon" href="./images/icon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <nav class="navbar" id="navbar">
+            <!--Menu-->
+            <div class="navbar-container container">
+                <input type="checkbox" name="" id="check">
+                <div class="hamburger-lines">
+                    <span class="line line1" id="open"></span>
+                    <span class="line line2" id="open"></span>
+                    <span class="line line3" id="open"></span>
+                </div>
+            </div>
+        </nav>
+
+        <div class="sidebar" id="menu">
+            <!--Links-->
+            <nav>
+                <ul class="list">
+                    <br> <br>
+                    <li class=""><a title="Home Page" class="home" href="#1" onclick="hide();">Home</a></li>
+                    <hr>
+                    <li class=""><a title="Services" class="home" href="#100" onclick="hide();">Services</a></li>
+                    <hr>
+                    <li class=""><a title="Find Doctors" class="find" href="login.php" onclick="hide();">Find Doctors</a></li>
+                    <hr>
+                    <li class=""><a  title="Help" class="home" href="#1" onclick="hide();">Help</a></li>
+                    <hr>
+                    <li class=""><a  title="Blog" class="home" href="#100" onclick="hide();">Blog</a></li>
+                    <hr>
+					<li class=""><a  title="Sign Up" class="home" href="./Register.php" onclick="hide();">Sign up</a></li>
+                    <hr>
+                </ul>
+            </nav>
+        </div>
+
+        <h2 class="logo" id="1" title="Health Service">Health</h2>
+        <a class="btn contact" href="./contact.html">Contact us</a>
+    </header>
+
+    <main class="main">
+        <!--First Page -->
+        <h1 class="subject">Unlocking the Secrets to a <span> Balanced Life </span></h1>
+        <p class="subp">Discover a world of well-being on our health-focused website. Take charge of your health
+            andtransform your life today!</p>
+
+        <div class="doctor">
+            <div class="shape">
+                <h3 class="hours">24/7 <span> Services </span></h3>
+            </div>
+        </div>
+        <a class="appointment" href="./appointment.html">Appointment</a>
+        
+        <a class="watch" href="https://www.youtube.com/">Watch Video</a>
+    </main>
+    <!-- Main end -->
+
+    <!-- Banner Start -->
+    <div class="banner">
+        <h1 class="ban">Discover the path to a Vibrant Life</h1>
+
+        <div class="container">
+            <div class="shape2">
+                <img class="profile" src="./images/06.png" alt="profile">
+                <a class="search1" href="./contact.html">Search Doctor</a>
+                <p class="search00"> Taking care of our bodies through regular exercise,</p>
+            </div>
+
+            <div class="shape3">
+                <img class="profile" src="./images/06.png" alt="profile">
+                <a class="search1" href="./index.html">Need Help</a>
+                <p class="search00"> Taking care of our bodies through regular exercise,</p>
+            </div>
+
+
+            <div class="shape4">
+                <img class="profile" src="./images/06.png" alt="profile">
+                <a class="search1" href="./index.html">Call a Doctor</a>
+                <p class="search00"> Taking care of our bodies through regular exercise,</p>
+            </div>
+
+            <div class="shape5">
+                <img class="profile" src="./images/06.png" alt="profile">
+                <a class="search1" href="./index.html">Location</a>
+                <p class="search00"> Taking care of our
+                    bodies through regular exercise,</p>
+
+            </div>
+        </div>
+    </div>
+    <!--Banner end-->
+    <!-- About Start -->
+    <div class="about">
+        <h3 class="about1">About us</h3>
+        <h1 class="quality">Quality health starts
+            with quality doctors</h1>
+        <p class="aboutp">We are dedicated to promoting a holistic approach to well-being, encompassing physical,
+            mental, and emotional health. Our team of experts is committed to providing you with reliable and up-to-date
+            information, tips, and resources to help you lead a healthier lifestyle. </p>
+        <div class="boxs">
+             <img class="image7" src="./images/03.png" alt="doctor"></div>
+    </div>
+    <!-- About end -->
+
+
+    <!--What we do start  -->
+
+    <div class="what" id="100">
+        <h1 class="what">What We Do?</h1>
+        <p class="whatp">We promote holistic health by providing expert guidance and valuable resources, empowering
+            individuals to lead a balanced and vibrant life. Join us to embrace well-being and make positive lifestyle
+            changes today!</p>
+        <div class="container2">
+            <div class="box">
+                <h2 class="call">Emergency Care</h2>
+                <p class="callp">Taking care of our bodies through regular exercise,
+                    nourishing diets, and adequate rest allows us to embrace
+                    life's challenges with vigor and resilience.</p>
+            </div>
+            <div class="box">
+                <h2 class="call">Dental Care</h2>
+                <p class="callp">Our team of experts is committed to providing you with reliable and up-to-date
+                    information, tips, and resources to help you lead a healthier lifestyle.</p>
+            </div>
+            <div class="box">
+                <h2 class="call">Heart Disease</h2>
+                <p class="callp">Taking care of our bodies through regular exercise,
+                    nourishing diets, and adequate rest allows us to embrace
+                    life's challenges with vigor and resilience.</p>
+            </div>
+        </div>
+    </div>
+    <!-- What End -->
+
+    <!-- Meet Start -->
+
+    <div class="meet">
+
+        <h1 class="sub2">Meet Our Specialist Doctors</h1>
+
+        <p class="sub2p">Our specialist doctors who are highly experienced and dedicated to providing exceptional
+            healthcare services. With their expertise and compassion, you can trust them to address your health
+            concerns and guide you towards a path of wellness and recovery.</p>
+
+        <div class="container3">
+
+            <div class="doctor1">
+                <label class="doctor1" for="">Dr.Fawad</label>
+
+            </div>
+
+            <div class="doctor2">
+                <label class="doctor2" for="">Dr.MD</label>
+            </div>
+
+
+            <div class="doctor3">
+                <label class="doctor3" for="">Dr.Hana</label>
+            </div>
+
+            <div class="doctor4">
+                <label class="doctor4" for="">Dr.Rahil</label>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    <!-- Comments Start -->
+    <div class="comments">
+      
+        <h4 class="patients">Happy Patients</h4>
+        <div class="no"></div>
+        <h1 class="patient0">Review From Our Patient</h1>
+        <div class="no"></div>
+        <div class="container4">
+            <div class="patient1">
+                <img class="patient1" src="./images/10.jpg" alt="an image">
+                <h4 class="patient1">Suleman Sadat</h4>
+            </div>
+            <div class="patient2">
+                <img class="patient2" src="./images/10.jpg" alt="an image">
+                <h4 class="patient2">Enayat Faiz</h4>
+            </div>
+            <div class="patient3">
+                <img class="patient3" src="./images/10.jpg" alt="an image">
+                <h4 class="patient3">Ahmad Joya</h4>
+            </div>
+        </div>
+        
+
+        <h4 class="title1">It is an Amazing Health Service</h4>
+        <img class="coimg" src="./images/Rate.png" alt="Rate">
+        <p class="comment1">I am highly impressed by the level of professionalism and care provided by the
+            specialist doctors. The wealth of expert guidance and valuable resources available on the platform
+            has empowered me to take control of my well-being. </p>
+    </div>
+
+    <!-- Subscribe -->
+    <div class="subscribe">
+
+        <div class="blue">
+            <form action="#" method="post">
+                <h3 class="">Subscribe Our Newsletters To Get
+                    the Best Medicine Advice </h3>
+
+                <input id="gmail" type="email" name="gmail" placeholder="Type your email">
+                <input class="subscribe" type="submit" name="Subscribe" value="Subscribe">
+            </form>
+        </div>
+
+
+
+    </div>
+
+    <!-- Subscribe End -->
+
+
+    <!--=============== Footer ===============-->
+    <footer>
+        <div class="container5">
+            <div class="wrapper">
+                <a href="#" class="logo"> Health</a>
+                <p class="footer-text">We are committed to your well-being and strive to provide top-notch care, expert
+                    guidance, and valuable resources to help you lead a healthier life.
+                </p>
+            </div>
+
+            <div class="wrapper">
+                <p class="footer-title">Quick Links</p>
+                <ul>
+                    <li>
+                        <a href="#" class="footer-link">Advertise with us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="wrapper">
+                <p class="footer-title">Legal Stuff</p>
+
+                <ul>
+                    <li>
+                        <a href="#" class="footer-link">Privacy Notice</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Cookie Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Terms Of Use</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="social">
+
+                <img class="wa" id="whatsapp" src="./images/whatsapp-logo-light.png" alt="whatsapp"
+                    onclick="whatsapp();">
+                <img class="tw" id="twitter" src="./images/twitter-logo-light.png" alt="twitter" onclick="twitter();">
+                <img class="insta" id="instagram" src="./images/instagram-logo-light.png" alt="instagram"
+                    onclick="instagram();">
+                <img class="youtube" id="youtube" src="./images/youtube-logo-light.png" alt="youtube"
+                    onclick="youtube();">
+            </div>
+            <div class="up">
+                <img class="up" id="opacity" src="./images/arrow-square-up-fill.png" alt="go to first page"
+                    onclick="up();">
+            </div>
+
+        </div>
+
+        <p class="copyright">&copy; Copyright 2023 <a href="#">Suleman Sadat</a></p>
+    </footer>
+    <!--=============== main script ===============-->
+    <script src="./scripts/script.js"></script>
+</body>
+
+</html>
